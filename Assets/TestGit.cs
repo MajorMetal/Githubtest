93e8fc992a973a329c7a806043ddf27f9a0f7f65
Matt Kinney <kinney.matthewl@gmail.com>
2018-03-12 14:25:33 -0700

    Git tisting

---
+++ Assets/TestGit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("testing the git process");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
